7f513d6fb2aba805266c9ffbe3920a8a3847e8ac
Alexander <ext_alexjimenez@falabella.cl>
2022-09-14 21:17:25 -0500

    api de personas

---
+++ main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"basesdedatos/mongo"
+	personas2 "basesdedatos/personas"
+	"github.com/gofiber/fiber/v2"
+)
+
+func main() {
+	app := fiber.New()
+	url := "mongodb+srv://admin:tueliges123@tueligescedula.i6zqf.mongodb.net/tueligescedula?retryWrites=true&w=majority"
+	mongoDatabase := mongo.NewDataBase(url, "personas")
+	dataBase := mongoDatabase.ConnectionDataBase()
+	personas := personas2.NewPersona(dataBase)
+
+	app.Post("/agente", func(c *fiber.Ctx) error {
+		agente := personas2.Agente{}
+		err := c.BodyParser(&agente)
+		if err != nil {
+			return c.SendString("error enviando la informacion por favor revise")
+		}
+
+		err = personas.CreateAgente(agente)
+		if err != nil {
+			return c.SendString("error guardando agente")
+		}
+
+		return c.SendString("datos guardados correctamente")
+	})
+
+	app.Get("/agentes", func(c *fiber.Ctx) error {
+		agentes, err := personas.VerAgentes()
+		if err != nil {
+			return c.SendString("error consultando agentes " + err.Error())
+		}
+
+		return c.JSON(agentes)
+	})
+
+	app.Get("/agente/:identificacion", func(c *fiber.Ctx) error {
+		id := c.Params("identificacion")
+		agente, err := personas.VerAgente(id)
+		if err != nil {
+			return c.SendString("error consultando el agente" + err.Error())
+		}
+
+		return c.JSON(agente)
+
+	})
+
+	app.Listen(":3000")
+}
